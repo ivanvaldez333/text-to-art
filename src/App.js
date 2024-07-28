743ac0745faded3a4f3b784c960760906d2c2863
IvanValdez3 <99561967+IvanValdez3@users.noreply.github.com>
2024-07-27 22:14:23 -0500

    Front end work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Header() { // This is a component/JSX element
+  return( 
+    <div id="header">
+      <h1 id="header-title">IMAGINA</h1>
     </div>
+  );
+}
+
+function App() {
+
+  const [name, SetName] = useState(""); // This is a hook
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(name);
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="App">
+
+        <br></br>
+
+        <form onSubmit={handleSubmit}>
+          <label id="prompt">How would you like your ad to look like?</label>
+          <input id="text-box" type="text" placeholder="Write your prompt here!" value={name} onChange={(e) => SetName(e.target.value)} />
+          <button onClick={() => alert(name)}>Submit</button>
+       </form>
+      </div>
+    </>
   );
 }
 
